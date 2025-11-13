3977976c3fcf4332ecb8aaceda723c7b0692a23f
Brian Spilsbury <brian.spilsbury@gmail.com>
2025-11-13 19:47:03 +0900

    Fix FS

--- geometry/getCgalNodeNative.js
+++ geometry/getCgalNodeNative.js
@@ -1,13 +1,13 @@
 import { createRequire } from 'node:module';
-import { stat, readFile, writeFile, mkdir } from 'node:fs/promises'; // Import async fs functions
+import nodeFS from 'node:fs';
 const require = createRequire(import.meta.url);
 
 // import { nativeNode } from './native.node.cjs';
 export const cgal = require('./native.node');
 
 export const FS = {
-  stat: (path) => stat(path),
-  readFile: (path, options) => readFile(path, options),
-  writeFile: (path, data, options) => writeFile(path, data, options),
-  mkdir: (path) => mkdir(path, { recursive: true }),
+  stat: (path) => nodeFS.statSync(path),
+  readFile: (path, options) => nodeFS.readFileSync(path, options),
+  writeFile: (path, data, options) => nodeFS.writeFileSync(path, data, options),
+  mkdir: (path) => nodeFS.mkdirSync(path, { recursive: true }),
 };
