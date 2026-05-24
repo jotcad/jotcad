46037f680226ab6eccfde3fbfb36d5369061bf24
Brian Spilsbury <brian.spilsbury@gmail.com>
2026-05-24 18:00:16 +0900

    fix(ci): integrate local CGAL and FreeType installers in install_deps.sh

--- geo/install_deps.sh
+++ geo/install_deps.sh
@@ -6,6 +6,19 @@ sudo apt-get install -y libboost-all-dev libssl-dev build-essential libxi-dev li
 
 echo "[Geo] Installing Local Project Dependencies..."
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Run CGAL local install script if cgal folder is not present
+if [ ! -d "$SCRIPT_DIR/cgal/cgal" ]; then
+    echo "[Geo] Running CGAL local installer..."
+    (cd "$SCRIPT_DIR/cgal" && ./install_cgal.sh)
+fi
+
+# Run FreeType local install script if freetype library is not present
+if [ ! -f "$SCRIPT_DIR/cgal/freetype/build/libfreetype.a" ]; then
+    echo "[Geo] Running FreeType local installer..."
+    (cd "$SCRIPT_DIR/cgal" && ./install_freetype.sh)
+fi
+
 "$SCRIPT_DIR/vendor/install_clipper.sh"
 "$SCRIPT_DIR/vendor/install_nlopt.sh"
 
